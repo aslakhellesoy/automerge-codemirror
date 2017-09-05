1d169bdba979c70c63059e2b2ba60953dd55c241
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-09-05 12:49:06 +0100

    Add fuzz tests

--- test/AutomergeCodeMirrorTest.js
+++ test/AutomergeCodeMirrorTest.js
@@ -49,5 +49,44 @@ describe('AutomergeCodeMirror', () => {
         'three\nevil\nrats\nsee\nhow\nthey\nrun\n'
       )
     })
+
+    for (let n = 0; n < 10; n++) {
+      it(`works with random edits (fuzz test ${n})`, () => {
+        cm.setValue(randomString(20))
+        for (let t = 0; t < 10; t++) {
+          monkeyType(cm)
+        }
+        assert.strictEqual(state.card.title.join(''), cm.getValue())
+      })
+    }
   })
 })
+
+function monkeyType(cm) {
+  const textLength = cm.getValue().length
+  const index = Math.floor(Math.random() * textLength)
+  const from = cm.posFromIndex(index)
+  const editLength = randomInt(10)
+  if (Math.random() < 0.7) {
+    // Add text
+    const text = randomString(editLength)
+    cm.replaceRange(text, cm.posFromIndex(index))
+  } else {
+    const endIndex = Math.max(index + editLength, textLength - 1)
+    const to = cm.posFromIndex(endIndex)
+    cm.replaceRange('', from, to)
+  }
+}
+function randomString(len) {
+  const chars =
+    '0123456789\nABCDEF\nGHIJKLM\nNOPQRSTUVWXTZ\nabcde\nfghiklmnop\nqrstuvwxyz'
+  let result = ''
+  for (let i = 0; i < len; i++) {
+    const rnum = randomInt(chars.length)
+    result += chars.substring(rnum, rnum + 1)
+  }
+  return result
+}
+function randomInt(max) {
+  return Math.floor(Math.random() * max)
+}
