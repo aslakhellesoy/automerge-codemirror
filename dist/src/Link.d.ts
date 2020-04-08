ea819fe9de665d6cbd2be1982b15b2581d36f02f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-04-08 17:34:39 +0100

    Remove dist folder

--- dist/src/Link.d.ts
+++
@@ -1,6 +0,0 @@
-import { Text } from 'automerge'
-import { Editor } from 'codemirror'
-export default interface Link<T> {
-  getText(doc: T): Text
-  codeMirror: Editor
-}
--- dist/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import AutomergeCodeMirror from './react/AutomergeCodeMirror'
-import automergeCodeMirror from './automergeCodeMirror'
-export { AutomergeCodeMirror, automergeCodeMirror }
