35061170a9e815608ac14cae079af3d01454264d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-04-13 00:06:25 +0100

    Avoid memory leak and outdated doc

--- src/react/useAutomergeDoc.ts
+++ src/react/useAutomergeDoc.ts
@@ -6,9 +6,15 @@ export default function useAutomergeDoc<D>(watchableDoc: Automerge.WatchableDoc<
 
   useEffect(() => {
     const handler = () => setDoc(watchableDoc.get())
+
+    // Because the useEffect hook is called asynchronously after render, there is
+    // a possibility that the watchableDoc changed between the render and the hook being called.
+    // We therefore call the handler once
+    handler()
+
     watchableDoc.registerHandler(handler)
     return () => watchableDoc.unregisterHandler(handler)
-  })
+  }, [])
 
   return doc
 }
